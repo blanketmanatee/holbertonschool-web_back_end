3ce485732ca54b7fa81fa8d0878989ae0b21617f
blanketmanatee <ashleyl.welch@gmail.com>
2021-10-19 13:14:40 -0500

    added sleep

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -11,5 +11,6 @@ async def wait_random(max_delay: int = 10) -> float:
     wait for random delay btwn 0 and 10 returns
     """
     actual_delay: float = random.uniform(0, max_delay)
+    await asyncio.sleep(actual_delay)
     return actual_delay
 
