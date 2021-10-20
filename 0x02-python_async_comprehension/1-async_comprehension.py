0ac52ad59d9e031d85d5572053ab328a2d0c25e7
blanketmanatee <ashleyl.welch@gmail.com>
2021-10-20 12:56:07 -0500

    complete except notes

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Async generator"""
+import asyncio
+import random
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    return [i async for i in async_generator()]
---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Async generator"""
+from asyncio import gather
+from time import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    first = time()
+    await gather(async_comprehension(), async_comprehension(),
+    async_comprehension(), async_comprehension())
+    second = time()
+    return second - first
