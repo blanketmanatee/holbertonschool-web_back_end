5877884e4519799977efbec47cb4728f4fa1bab6
blanketmanatee <ashleyl.welch@gmail.com>
2021-10-21 10:36:58 -0500

    indents

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -8,6 +8,6 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     first = time()
     await gather(async_comprehension(), async_comprehension(),
-        async_comprehension(), async_comprehension())
+            async_comprehension(), async_comprehension())
     second = time()
     return second - first
