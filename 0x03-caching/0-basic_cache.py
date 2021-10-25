78860ff12bdc8469610bb216d3409c4e5305b9ee
blanketmanatee <ashleyl.welch@gmail.com>
2021-10-25 09:11:56 -0500

    notes

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -9,9 +9,11 @@ class BasicCache(BaseCaching):
     and is a caching system.
     """
     def put(self, key, item):
+        """more notes"""
         if key and item:
             self.cache_data[key] = item
     def get(self, key):
+        """more notes"""
         if key is None or self.cache_data.get(key) is None:
             return None
         return self.cache_data.get(key)
