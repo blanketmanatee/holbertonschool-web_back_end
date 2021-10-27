c16fbf51e71cf2f836c5c0f535e4c0d3069cc1fd
blanketmanatee <ashleyl.welch@gmail.com>
2021-10-27 09:47:50 -0500

    missing whitepace

--- 0x04-pagination/0-simple_helper_function.py
+++ 0x04-pagination/0-simple_helper_function.py
@@ -3,7 +3,7 @@
 from typing import Tuple
 
 
-def index_range(page: int, page_size:int) -> Tuple[int, int]:
+def index_range(page: int, page_size:int) -> Tuple[int, int]: 
     """returns a tuple of size two containing start and ending
     index corresponding to range of indexes to return in a list """
     return ((page - 1) * page_size, page * page_size)
