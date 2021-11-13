1a1319773723b4c44088fbd0c6cc08b412574eb9
blanketmanatee <ashleyl.welch@gmail.com>
2021-11-13 17:48:30 -0600

    fixing 400s

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -3,6 +3,8 @@
 Route module for the API
 """
 from os import getenv
+
+from flask.globals import current_app
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
@@ -24,25 +26,6 @@ if getenv('AUTH_TYPE') == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth
 
-@app.before_request
-def before_request() -> str:
-    """ before each req return str or nothing"""
-    if auth is None:
-        return
-
-    expath = ['/api/v1/status',
-              '/api/v1/unauthorized/',
-              '/api/v1/forbidden/']
-    if not (auth.require_auth(request.path, expath)):
-        return
-
-    if (auth.authorization_header(request)) is None:
-        abort(401)
-
-    if (auth.current_user(request)) is None:
-        abort(403)
-
-
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
@@ -61,6 +44,18 @@ def forbidden(error) -> str:
     """ handle http 403 """
     return jsonify({"error": "Forbidden"}), 403
 
+@app.before_request
+def before_request() -> str:
+    """ before each req return str or nothing"""
+
+    expath = ['/api/v1/status',
+              '/api/v1/unauthorized/',
+              '/api/v1/forbidden/']
+    if auth and auth.require_auth(request.path, path_list):
+        if not auth.authorization_header(request):
+            abort(401
+        if not auth.current_user(request):
+            abort(403)
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
