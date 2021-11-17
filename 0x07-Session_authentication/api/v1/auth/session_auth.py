073d12caed20326d18d142abfa8c6246a28ddc80
blanketmanatee <ashleyl.welch@gmail.com>
2021-11-17 12:20:58 -0600

    corrected typos for user_id

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -12,11 +12,11 @@ class SessionAuth(Auth):
 
     def create_session(self, user_id: str = None) -> str:
         """ create a session and register in class"""
-        if user_id is None or type(User) is not str:
+        if user_id is None or type(user_id) is not str:
             return None
 
         session_id: str = str(uuid4())
-        self.user_id_by_session_id[session_id] = User
+        self.user_id_by_session_id[session_id] = user_id
 
         return session_id
 
