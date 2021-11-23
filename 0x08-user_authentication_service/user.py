1b572a8633397a3a8db83822918af81dd8ff909d
blanketmanatee <ashleyl.welch@gmail.com>
2021-11-22 23:15:52 -0600

    white space added

--- 0x08-user_authentication_service/user.py
+++ 0x08-user_authentication_service/user.py
@@ -8,7 +8,7 @@ Base = declarative_base()
 
 class User(Base):
     """SQLAlchemy database table named users"""
-    __tablename__= "users"
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=True)
