1ad0b9c984fac37b0612f24ccce5b3a5cac1942e
blanketmanatee <ashleyl.welch@gmail.com>
2021-11-23 14:00:34 -0600

    checker

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -25,9 +25,9 @@ class TestGithubOrgClient(unittest.TestCase):
     def test_public_repos_url(self):
         """test client"""
         with patch.object(GithubOrgClient,
-                         "org",
-                         new_callable=PropertyMock,
-                         return_value={"repos_url": "holberton"}) as mock_get:
+                          "org",
+                          new_callable=PropertyMock,
+                          return_value={"repos_url": "holberton"}) as mock_get:
             test_json = {"repost_url": "holberton"}
             test_client = GithubOrgClient(test_json.get("repos_url"))
             test_return = test_client._public_repos_url
@@ -39,14 +39,14 @@ class TestGithubOrgClient(unittest.TestCase):
     def test_public_repos(self, mock_get):
         """unit tests client"""
         with patch.object(GithubOrgClient,
-                        "_public_repos_url",
-                        new_callable=PropertyMock,
-                        return_value="https://api.github.com/") as mock_pub:
-                    test_client = GithubOrgClient("holberton")
-                    test_return = test_client.publick_repos()
-                    self.assertEqual(test_return, ["holberton"])
-                    mock_get.assert_called_once
-                    mock_pub.assert_called_once
+                         "_public_repos_url",
+                         new_callable=PropertyMock,
+                         return_value="https://api.github.com/") as mock_pub:
+                test_client = GithubOrgClient("holberton")
+                test_return = test_client.publick_repos()
+                self.assertEqual(test_return, ["holberton"])
+                mock_get.assert_called_once
+                mock_pub.assert_called_once
     @parameterized.expand([parameterized({"license": {"key": "my_license"}}, "my_license, True"),
     ({"license": {"key": "other_license"}}, "my_license", False),
     ])
