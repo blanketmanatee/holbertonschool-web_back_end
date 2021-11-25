227a35cce3710c96da5355d76a59f7a8c40c6c28
blanketmanatee <ashleyl.welch@gmail.com>
2021-11-24 22:57:59 -0600

    task 4

---
+++ 0x0A-i18n/4-app.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""Setup Babel"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext
+from os import getenv
+
+app = Flask(__name__)
+babel = Babel(app)
+"""init Babel obj"""
+
+
+class Config(object):
+    """Class Config"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+"""config for flask"""
+
+
+@app.route('/')
+def route():
+    """flask app"""
+    return render_template("3-index.html")
+
+
+@babel.localeselector
+def get_locale():
+    """get locale"""
+    lang = request.args.get('locale')
+    supportLang = app.config['LANGUAGES']
+    if lang in supportLang:
+        return lang
+    else:
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
