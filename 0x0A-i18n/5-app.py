e096177cd8412e1e55961ecfebf18399055f8163
blanketmanatee <ashleyl.welch@gmail.com>
2021-11-25 14:34:39 -0600

    pycodestyles

--- 0x0A-i18n/5-app.py
+++ 0x0A-i18n/5-app.py
@@ -51,10 +51,12 @@ def get_user():
     except Exception:
         return None
 
+
 @app.before_request
 def before_request():
     """find a user if exists"""
     g.user = get_user()
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
