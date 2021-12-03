83572f28d3543aafb43a2e2eb3788bb542696c63
blanketmanatee <ashleyl.welch@gmail.com>
2021-12-02 21:47:06 -0600

    advanced mysql

--- 0x0C-MySQL_Advanced/6-bonus.sql
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -3,13 +3,12 @@ DELIMITER |
 CREATE procedure AddBonus (
     IN user_id int,
     IN project_name VARCHAR(255),
-    IN score float)
+    IN score int)
 BEGIN
     INSERT INTO projects (name)
     SELECT project_name FROM DUAL
 
-    WHERE NOT EXISTS (SELECT * FROM projects WHERE name = project_name);
+    WHERE NOT EXISTS (SELECT * FROM projects WHERE name = project_name LIMIT 1);
     INSERT INTO corrections (user_id, project_id, score)
     VALUES (user_id, (SELECT id FROM projects WHERE name = project_name), score);
-    END;
-|
+    END |
