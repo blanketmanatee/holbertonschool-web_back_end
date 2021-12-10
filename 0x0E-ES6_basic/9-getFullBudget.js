eb416a0a65119d0423f64573ef02d6a2e5e240f9
blanketmanatee <ashleyl.welch@gmail.com>
2021-12-09 21:25:31 -0600

    checker

--- 0x0E-ES6_basic/9-getFullBudget.js
+++ 0x0E-ES6_basic/9-getFullBudget.js
@@ -2,7 +2,7 @@ import getBudgetObject from './7-getBudgetObject';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
-  const fullBudget= {
+  const fullBudget = {
     ...budget,
     getIncomeInDollars(income) {
       return `$${income}`;
