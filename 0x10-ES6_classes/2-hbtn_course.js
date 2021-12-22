1b019b21277f1135a316c549a1fd554253222916
blanketmanatee <ashleyl.welch@gmail.com>
2021-12-22 16:05:19 -0600

    task 2

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -33,7 +33,7 @@ export default class HolbertonCourse {
   }
 
   set students(newStudents) {
-      if (newStudents.constructor !== Array && newStudents.every((el) => typeof el ==== 'string')) {
+      if (newStudents.constructor !== Array && newStudents.every((el) => typeof el === 'string')) {
           throw TypeError ('students must be an array of strings');
       }
     this._students = newStudents;
