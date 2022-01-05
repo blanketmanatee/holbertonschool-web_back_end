0900756c63253704f4b165d8cb6640beacdf1cba
blanketmanatee <ashleyl.welch@gmail.com>
2022-01-05 15:30:01 -0600

    through chai

---
+++ 0x13-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,25 @@
+'use strict';
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber type == SUM', () => {
+    it('checks output for SUM', () => {
+        assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+        assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
+    });
+});
+
+describe('calculateNumber type == SUBTRACT', () => {
+    it('checks output of SUBTRACT', () => {
+        assert.strictEqual(calculateNumber('SUBTRACT', 4, 3), 1);
+        assert.strictEqual(calculateNumber('SUBTRACT', 2, 4.5), -3);
+    });
+});
+
+describe('calculateNumber type == DIVIDE', () => {
+    it('checks output for DIVIDE', () => {
+        assert.strictEqual(calculateNumber('DIVIDE', 0.0, 2), 0);
+        assert.strictEqual(calculateNumber('DIVIDE', -1, 1), -1);
+        assert.strictEqual(calculateNumber('DIVIDE', 1, 0), 'Error');
+    });
+});
---
+++ 0x13-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,25 @@
+'use strict';
+const chai = require('chai');
+const calculateNumber = require('./2-calcul_chai');
+
+describe('calculateNumber type == SUM', () => {
+    it('checks output for SUM', () => {
+        chai.expect(calculateNumber('SUM', 1.4, 4.5), 6);
+        chai.expect(calculateNumber('SUM', 1, 3), 4);
+    });
+});
+
+describe('calculateNumber type == SUBTRACT', () => {
+    it('checks output of SUBTRACT', () => {
+        chai.expect(calculateNumber('SUBTRACT', 4, 3), 1);
+        chai.expect(calculateNumber('SUBTRACT', 2, 4.5), -3);
+    });
+});
+
+describe('calculateNumber type == DIVIDE', () => {
+    it('checks output for DIVIDE', () => {
+        chai.expect(calculateNumber('DIVIDE', 0.0, 2), 0);
+        chai.expect(calculateNumber('DIVIDE', -1, 1), -1);
+        chai.expect(calculateNumber('DIVIDE', 1, 0), 'Error');
+    });
+});
