880a44e38c6b5e75d53ca7650c787e6fef3d3e2d
blanketmanatee <ashleyl.welch@gmail.com>
2022-01-05 23:11:24 -0600

    utils

--- 0x13-unittests_in_js/utils.js
+++ 0x13-unittests_in_js/utils.js
@@ -1,9 +1,17 @@
 'use strict';
 const Utils = {
-    calculateNumber(type, a, b) {
-    if (type === 'SUM') return Math.round(a)+ Math.round(b);
-    if (type === 'SUBTRACT') return Math.round(a) - Math.round(b);
-    if (type === 'DIVIDE') return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
+    calculateNumber: function (type, a, b) {
+    switch (type) {
+        case 'SUM':
+            return Math.round(a) + Math.round(b);
+        case 'SUBTRACT':
+            return Math.round(a) - Math.round(b);
+        case 'DIVIDE':
+            if (Math.round(b) === 0) {
+                return 'Error';
+            }
+            return Math.round(a) / Math.round(b);
+        }
     }
 }
 module.exports = Utils;
