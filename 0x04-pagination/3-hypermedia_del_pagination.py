2b2cf9f6471e1b4dcc1a30c50f3957035352daff
blanketmanatee <ashleyl.welch@gmail.com>
2022-01-08 14:37:33 -0600

    spacing

--- 0x04-pagination/3-hypermedia_del_pagination.py
+++ 0x04-pagination/3-hypermedia_del_pagination.py
@@ -40,8 +40,10 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-            """Implement a get_hyper_index method with two integer arguments: index
-            with a None default value and page_size with default value of 10."""
+            """Implement a get_hyper_index method
+            with two integer arguments: index
+            with a None default value and page_size
+            with default value of 10."""
             assert index >= 0 and index < len(self.__indexed_dataset)
 
             data = []
