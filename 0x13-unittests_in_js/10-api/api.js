6416fb15ed5e73810a07f315de35f344b1bdc8ba
blanketmanatee <ashleyl.welch@gmail.com>
2022-01-08 20:45:30 -0600

    task 10

--- 0x13-unittests_in_js/10-api/api.js
+++ 0x13-unittests_in_js/10-api/api.js
@@ -1,6 +1,12 @@
-{
-    payment_methods: {
-      credit_cards: true,
-      paypal: false
-    }
-  }
+const express = require('express');
+const app = express();
+const port = 7865;
+
+app.get('/', (req, res) => {
+    res.status(200);
+    res.send('Welcome to the payment system');
+});
+
+app.listen(port, () => {
+    console.log('API available on localhost port 7865');
+});
