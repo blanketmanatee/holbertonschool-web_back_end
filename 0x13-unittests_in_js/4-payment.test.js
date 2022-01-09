896fe3722c485f8940a6d18fdee65e1163439f2d
blanketmanatee <ashleyl.welch@gmail.com>
2022-01-08 20:23:27 -0600

    api

--- 0x13-unittests_in_js/4-payment.test.js
+++ 0x13-unittests_in_js/4-payment.test.js
@@ -3,22 +3,16 @@ const chai = require('chai');
 const sinon = require('sinon');
 
 const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./4-payment');
+const sendPaymentRequestToApi = require('./3-payment');
 const { spy } = require('sinon');
 
-describe('sendPaymentRequestToApi', () => {
-    it('call calculateNumber', () => {
-        const stub = sinon.stub(utils, 'calculateNumber');
-        stub returns(10);
+describe('sendPaymentRequestToApi function', () => {
+    const spyUtils = sinon.spy(Utils, 'calculateNumber');
 
-        const spyConsole = sinon.spy(console, 'log');
-
-        const stubUtils = sinon.stub(Utils, 'calculateNumber');
-        stubUtils.withArgs('SUM', 100, 20).returns(10);
+    it('validate usage of Utils', () => {
         sendPaymentRequestToApi(100, 20);
         chai.expect(spyUtils.calledOnce).to.be.true;
         chai.expect(spyUtils.calledWith('SUM', 100, 20)).to.be.true;
-        stubUtils.restore()
-        spyConsole.restore();
+        spyUtils.restore()
     });
 });
