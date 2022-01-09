45bed15160bcad1e345f171a0767dc52b9b65ea2
blanketmanatee <ashleyl.welch@gmail.com>
2022-01-08 20:26:57 -0600

    api task 9

--- 0x13-unittests_in_js/9-api/api.js
+++ 0x13-unittests_in_js/9-api/api.js
@@ -1,3 +1,16 @@
-Add a new endpoint: GET /cart/:id
-:id must be only a number (validation must be in the route definition)
-When access, the endpoint should return Payment methods for cart :id
+const express = require('express');
+const app = express();
+const port = 7865;
+
+app.get('/', (req, res) => {
+    res.status(200);
+    res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id([0-9]*)', (req, res) => {
+    res.send(`Payment methods for cart${req.params.id}`);
+});
+
+app.listen(port, () => {
+    console.log('API available on localhost port 7865');
+});
