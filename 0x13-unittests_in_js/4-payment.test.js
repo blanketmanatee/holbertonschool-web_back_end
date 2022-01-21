9dc3b1e424b66b98113f481c28b8001d85011572
blanketmanatee <ashleyl.welch@gmail.com>
2022-01-20 20:47:18 -0600

    spy and stub

--- 0x13-unittests_in_js/4-payment.test.js
+++ 0x13-unittests_in_js/4-payment.test.js
@@ -1,10 +1,11 @@
 'use strict';
 const chai = require('chai');
 const sinon = require('sinon');
-const {spy, stub } = require('sinon');
+
 
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./4-payment');
+const { spy } = require('sinon');
 
 describe('sendPaymentRequestToApi function', () => {
     const spyUtils = sinon.spy(Utils, 'calculateNumber');
