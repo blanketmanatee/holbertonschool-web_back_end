a2c06578acdedbbf76a0281fdc2d75fdabf77d02
blanketmanatee <ashleyl.welch@gmail.com>
2022-01-21 08:56:52 -0600

    style

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -9,4 +9,3 @@ def concat(str1: str, str2: str) -> str:
     concatenate two strings
     """
     return (str1 + str2)
-    
--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -7,8 +7,8 @@ from typing import TypeVar, Mapping, Any, Union
 T = TypeVar('T')
 
 
-def safely_get_value(dct:Mapping, key:Any, default:Union[T,None] = None)\
-    -> Union[Any, T]:
+def safely_get_value(dct: Mapping, key: Any, default: Union[T,None] = None)\
+            -> Union[Any, T]:
     """
     Returns any or T
     """
