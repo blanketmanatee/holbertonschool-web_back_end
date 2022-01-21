feedec0ccec62026d6443d5c767d4f6e459e8ffb
blanketmanatee <ashleyl.welch@gmail.com>
2022-01-20 21:26:06 -0600

    task 5

--- 0x0A-i18n/5-app.py
+++ 0x0A-i18n/5-app.py
@@ -3,6 +3,7 @@
 from flask import Flask, render_template, request, g
 from flask_babel import Babel, gettext
 from os import getenv
+from typing import Union
 
 app = Flask(__name__)
 babel = Babel(app)
@@ -22,11 +23,11 @@ class Config(object):
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-app.config.from_object(Config)
+app.config.from_object('5-app.Config')
 """config for flask"""
 
 
-@app.route('/')
+@app.route('/', methods=['GET'], strict_slashes=False)
 def route():
     """flask app"""
     return render_template("5-index.html")
